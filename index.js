6745f98e3ffb05cbdf9bdd7c516e7982d629c6f5
Daven Quinn <dev@davenquinn.com>
2018-01-29 00:30:00 -0800

    Added function to find symbols in node js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = __dirname
+var baseDir = __dirname;
+var path = require('path');
+
+module.exports.baseDir = __dirname;
+module.exports.findSymbol = function(fn){
+  var ext = path.extname(fn).slice(1);
+  return path.join(__dirname,'assets', ext, fn)
+};
+
